902a933e15afa0f7728b7bca018259bc471d1cbb
redoudi <135809071+redoudi@users.noreply.github.com>
2025-10-16 12:20:25 +0200

    ki

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,21 +4,10 @@ import React from "react";
 import { HapticTab } from "@/components/haptic-tab";
 import { Colors } from "@/constants/theme";
 import { useColorScheme } from "@/hooks/use-color-scheme";
-import { Image, StyleSheet } from "react-native";
-
-const TabBarItemIcon = (imgSource) => (
-  <Image
-    source={imgSource}
-    resizeMode="stretch"
-    style={{ width: 48, height: 48, marginBottom: 16 }}
-  />
-);
+import { Image, StyleSheet, View } from "react-native";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-
-  const heeziIconSource = require("../../assets/heezi.png");
-  const playIconSource = require("../../assets/play.png");
 
   return (
     <Tabs
@@ -27,41 +16,33 @@ export default function TabLayout() {
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarPosition: "left",
-        tabBarLabelStyle: {
-          color: "#292929",
-          fontSize: 24,
-          fontWeight: "bold",
-        },
-        tabBarIconStyle: {
-          width: 48,
-          height: 48,
-          marginRight: 16,
-        },
+        tabBarStyle: styles.tabBarStyle,
+        tabBarItemStyle: styles.tabBarItemStyle,
+        tabBarIconStyle: styles.tabBarIconStyle,
+        tabBarLabelStyle: styles.tabBarLabelStyle,
+
+        tabBarBackground: () => (
+          <View style={styles.tabBarBackgroundStyle}>
+            <Image
+              source={{
+                uri: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/ROUqyFKGQX/qih0dnn3_expires_30_days.png",
+              }}
+              resizeMode="stretch"
+              style={styles.logoStyle}
+            />
+          </View>
+        ),
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
-          title: "",
-          tabBarIcon: () => (
-            <Image
-              source={require("../../assets/heezi.png")}
-              resizeMode="stretch"
-              style={{ width: 191, height: 69, marginBottom: 32 }}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="jouer"
-        options={{
           title: "Jouer",
-          href: "/(tabs)/jouer",
           tabBarIcon: () => (
             <Image
               source={require("../../assets/play.png")}
               resizeMode="stretch"
-              style={styles.tabBarItemIcon}
+              style={styles.tabBarItemIconImage}
             />
           ),
         }}
@@ -75,7 +56,7 @@ export default function TabLayout() {
             <Image
               source={require("../../assets/profil.png")}
               resizeMode="stretch"
-              style={styles.tabBarItemIcon}
+              style={styles.tabBarItemIconImage}
             />
           ),
         }}
@@ -89,7 +70,7 @@ export default function TabLayout() {
             <Image
               source={require("../../assets/score.png")}
               resizeMode="stretch"
-              style={styles.tabBarItemIcon}
+              style={styles.tabBarItemIconImage}
             />
           ),
         }}
@@ -103,7 +84,7 @@ export default function TabLayout() {
             <Image
               source={require("../../assets/succes.png")}
               resizeMode="stretch"
-              style={styles.tabBarItemIcon}
+              style={styles.tabBarItemIconImage}
             />
           ),
         }}
@@ -113,9 +94,46 @@ export default function TabLayout() {
 }
 
 const styles = StyleSheet.create({
-  tabBarItemIcon: {
+  tabBarStyle: {
+    width: 193,
+    marginTop: 32,
+    backgroundColor: "white",
+    elevation: 0,
+    shadowOpacity: 0,
+    paddingTop: 101,
+    borderWidth: 1,
+    borderColor: "red",
+    alignItems: "center",
+  },
+  tabBarBackgroundStyle: {
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logoStyle: {
+    width: 191,
+    height: 69,
+  },
+  tabBarItemStyle: {
+    alignSelf: "flex-start",
+    flexDirection: "row",
+    alignItems: "center",
+    borderRadius: 8,
+    paddingBottom: 24,
+  },
+  tabBarIconStyle: {
     width: 48,
     height: 48,
-    marginRight: 16,
+    marginRight: 14,
+  },
+
+  tabBarLabelStyle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "black",
+  },
+  tabBarItemIconImage: {
+    width: 48,
+    height: 48,
   },
 });
