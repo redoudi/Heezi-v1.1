9834b427dfe10ccb507609b74797fc3a3537edfc
redoudi <135809071+redoudi@users.noreply.github.com>
2025-10-18 12:42:53 +0200

    ok

--- components/home/category-list.tsx
+++ components/home/category-list.tsx
@@ -1,17 +1,46 @@
 import { CategoryCard } from "@/components/home/category-card";
-import { categories } from "@/constants/categories";
-import { StyleSheet, View } from "react-native";
+
+import usePracticeTool, { PracticeTool } from "@/context/usePracticeTool";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+
+export interface CategoryItem {
+  title: string;
+  backgroundColor: string;
+  textColor: string;
+  tool: PracticeTool;
+}
+
+export const categories: CategoryItem[] = [
+  {
+    title: "Gestion d'un tableur",
+    backgroundColor: "#72D6BA",
+    textColor: "#0A2924",
+    tool: "spreadsheet",
+  },
+  {
+    title: "Edition de texte",
+    backgroundColor: "#33C6FD",
+    textColor: "#0A4F70",
+    tool: "textEditor",
+  },
+];
 
 export function CategoryList() {
+  const { setPracticeTool } = usePracticeTool();
   return (
     <View style={styles.categoryList}>
       {categories.map((category, index) => (
-        <CategoryCard
+        <TouchableOpacity
           key={index}
-          title={category.title}
-          backgroundColor={category.backgroundColor}
-          textColor={category.textColor}
-        />
+          onPress={() => setPracticeTool(category.tool)}
+        >
+          <CategoryCard
+            key={index}
+            title={category.title}
+            backgroundColor={category.backgroundColor}
+            textColor={category.textColor}
+          />
+        </TouchableOpacity>
       ))}
     </View>
   );
