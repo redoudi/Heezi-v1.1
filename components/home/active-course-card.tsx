9273e20768eb95d55cd3be226b1df2971149daa9
redoudi <135809071+redoudi@users.noreply.github.com>
2025-10-21 11:09:45 +0200

    k

--- components/home/active-course-card.tsx
+++ components/home/active-course-card.tsx
@@ -1,14 +1,20 @@
 import { CourseCard } from "@/components/home/course-card";
+import usePracticeTool from "@/context/usePracticeTool";
 
 export function ActiveCourseCard() {
+  const { practiceTool } = usePracticeTool();
   return (
     <CourseCard
       thumbnailUri="https://storage.googleapis.com/tagjs-prod.appspot.com/v1/ROUqyFKGQX/ldtg9pud_expires_30_days.png"
       progress="6 Sections/10 niveaux"
       status="En cours"
-      title="Gestion d'un tableur"
+      title={
+        practiceTool === "spreadsheet"
+          ? "Gestion d'un tableur"
+          : "L'éditeur de texte"
+      }
       description="Lörem ipsum sulingar sasade, om än säv. "
-      onPress={() => alert("Pressed!")}
+      onPress={() => {}}
     />
   );
 }
