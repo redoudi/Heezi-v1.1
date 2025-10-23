81b45ddd878f261d96a7f645808413020a1ac5d6
redoudi <135809071+redoudi@users.noreply.github.com>
2025-10-23 15:21:25 +0200

    k

--- app/sandbox.tsx
+++ app/sandbox.tsx
@@ -55,7 +55,7 @@ const Mascot = () => {
   );
 };
 
-export default function Sandbox() {
+export default function MascotDialog() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.mainRow}>
