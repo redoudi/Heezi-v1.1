feb6b21f8acc3152199d8b1d042a700444aacb6c
redoudi <135809071+redoudi@users.noreply.github.com>
2025-10-27 14:48:45 +0100

    k

--- hooks/useCheckCondition.ts
+++ hooks/useCheckCondition.ts
@@ -6,17 +6,20 @@ export default function useCheckCondition({
   stepExpectedRef: any;
 }) {
   const {
-    spreadsheetData: { cellValues, cellsSelected },
+    spreadsheetData: { cellsValues, cellsSelected, cellsStyles },
   } = useSpreadsheetStore();
   return () => {
     switch (stepExpectedRef.current.type) {
       case "cellValue":
-        const currentValue = cellValues[stepExpectedRef.current.cell];
+        const currentValue = cellsValues[stepExpectedRef.current.cell];
         return currentValue == stepExpectedRef.current.value;
       case "cellSelected":
         return cellsSelected.includes(stepExpectedRef.current.cell);
-      case "cellProps":
-        return false;
+      case "cellStyle":
+        return (
+          JSON.stringify(cellsStyles?.[stepExpectedRef.current.cell]) ===
+          JSON.stringify(stepExpectedRef.current.style)
+        );
     }
   };
 }
