5be7c26c7a250bc6d384cccff440c4a8401d0d59
redoudi <135809071+redoudi@users.noreply.github.com>
2025-10-30 13:30:01 +0100

    w

--- app/mission/[id]/practice.tsx
+++ app/mission/[id]/practice.tsx
@@ -1,15 +1,18 @@
 import MascotMonitor from "@/components/mascot/mascot-monitor";
+import QuizScreen from "@/components/practice-tools/quiz/quiz";
 import SpreadsheetScreen from "@/components/practice-tools/spreadsheet/SpreadsheetScreen";
 import TextEditorScreen from "@/components/practice-tools/textEditor";
 import usePracticeTool from "@/context/usePracticeTool";
 import useLoadLevelData from "@/hooks/useLoadLevelData";
-import { useLocalSearchParams } from "expo-router";
 import { View } from "react-native";
 
 export default function PracticeToolScreen() {
-  const { id } = useLocalSearchParams();
   const { practiceTool } = usePracticeTool();
-  useLoadLevelData();
+  const { levelType } = useLoadLevelData();
+
+  if (levelType === "quiz") {
+    return <QuizScreen />;
+  }
 
   const PracticeToolScreen = () => {
     switch (practiceTool) {
--- hooks/useLoadLevelData.ts
+++ hooks/useLoadLevelData.ts
@@ -2,16 +2,19 @@ import { levelFiles } from "@/assets/levels/indexLevels";
 import usePracticeTool from "@/context/usePracticeTool";
 import useSpreadsheetStore from "@/store/useSpreadsheetStore";
 import { useLocalSearchParams } from "expo-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function useLoadLevelData() {
   const { id } = useLocalSearchParams();
   const { practiceTool } = usePracticeTool();
   const { setLevelData } = useSpreadsheetStore();
+  const [levelData, setLevelDataState] = useState<any>(null);
   useEffect(() => {
     if (id && practiceTool && setLevelData) {
       const levelData = levelFiles[practiceTool][id as string];
+      setLevelDataState(levelData);
       setLevelData(levelData);
     }
   }, [practiceTool, id, setLevelData]);
+  return levelData;
 }
