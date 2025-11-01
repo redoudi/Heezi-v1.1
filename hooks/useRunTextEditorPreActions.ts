198b42392f9fb4a3dee6caa51b6754c8d34297c5
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-01 20:16:02 +0100

    ok

--- hooks/useRunTextEditorPreActions.ts
+++ hooks/useRunTextEditorPreActions.ts
@@ -1,11 +1,18 @@
 import useTextEditorStore from "@/store/useTextEditorStore";
 
 export default function useRunTextEditorPreActions() {
-  const { setBlockText } = useTextEditorStore();
+  const { setBlockText, setSelectedBlockIndex, setBlockStyle } =
+    useTextEditorStore();
   const runPreAction = (preAction: any) => {
     switch (preAction.type) {
       case "blockText":
         setBlockText(Number(preAction.blockIndex), preAction.text);
+        break;
+      case "selectBlock":
+        setSelectedBlockIndex(Number(preAction.blockIndex));
+        break;
+      case "style":
+        setBlockStyle(Number(preAction.blockIndex), preAction.value);
         break;
     }
   };
