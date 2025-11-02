682a726a91946a7a015c8b67a17eb00cb40e5a5e
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-02 19:34:33 +0100

    k

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,15 +5,9 @@ import { SafeAreaView, StyleSheet, View } from "react-native";
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.view}>
-        <View style={styles.row}>
-          <View style={styles.mainContent}>
-            <View style={styles.contentRow}>
-              <LeftColumn />
-              <RightColumn />
-            </View>
-          </View>
-        </View>
+      <View style={styles.contentRow}>
+        <LeftColumn />
+        <RightColumn />
       </View>
     </SafeAreaView>
   );
@@ -25,18 +19,7 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
     backgroundColor: "#FFFFFF",
   },
-  view: {
-    width: 1440,
-    backgroundColor: "#FFFFFF",
-  },
-  row: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  mainContent: {
-    width: 1121,
-    marginRight: 32,
-  },
+
   contentRow: {
     flexDirection: "row",
     marginBottom: 16,
