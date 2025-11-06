6f60e5ce1b9af72400260527c13bb165aad73576
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-06 20:53:17 +0100

    k

--- app/mission/[practiceTool]/[id]/index.tsx
+++ app/mission/[practiceTool]/[id]/index.tsx
@@ -103,7 +103,7 @@ const styles = StyleSheet.create({
   avatar: {
     borderRadius: 8,
     alignSelf: "center",
-    minHeight: 400,
+    minHeight: 200,
   },
   introTextBox: {
     alignSelf: "flex-start",
