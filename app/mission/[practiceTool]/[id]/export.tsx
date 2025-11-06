bd1b1f6729b5d4f7d5d6a7a99ae97c5dc7edf883
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-06 20:50:09 +0100

    k

--- app/mission/[practiceTool]/[id]/export.tsx
+++ app/mission/[practiceTool]/[id]/export.tsx
@@ -40,7 +40,7 @@ export default function ExportScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "flex-start",
+    alignItems: "center",
     backgroundColor: "#FFFFFF",
   },
   buttonRow5: {
