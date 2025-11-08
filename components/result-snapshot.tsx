56e3c7338a679425c0e4616fea9184cfe268f7a7
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-08 11:51:14 +0100

    w

--- components/result-snapshot.tsx
+++ components/result-snapshot.tsx
@@ -37,7 +37,6 @@ export default function ResultSnapshot() {
               ))}
             </View>
           </View>
-
           <FlatList
             style={{ borderWidth: 1, borderColor: "black" }}
             data={entries}
