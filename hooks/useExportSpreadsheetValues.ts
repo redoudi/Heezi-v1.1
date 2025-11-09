ec4c709457172a29943fcdff2580d2841a606041
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-09 11:28:19 +0100

    k

--- hooks/useExportSpreadsheetValues.ts
+++ hooks/useExportSpreadsheetValues.ts
@@ -1,8 +1,9 @@
 import useSpreadsheetStore from "@/store/useSpreadsheetStore";
 import { rangeToCells } from "@/utils/spreadsheetUtils";
+import useLoadSpreadsheet from "./useLoadSpreadsheet";
 
 export default function useExportSpreadsheetValues() {
-  // useLoadSpreadsheet();
+  useLoadSpreadsheet();
   const {
     spreadsheetData: { cellsValues, cellsStyles },
   } = useSpreadsheetStore();
