2e3be845ffd4328e3b4a05a9ff3ae14f0d723324
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-15 19:31:39 +0100

    k

--- context/usePracticeTool.tsx
+++ context/usePracticeTool.tsx
@@ -5,15 +5,31 @@ export type PracticeTool = "spreadsheet" | "textEditor" | "";
 const PracticeToolContext = createContext<{
   practiceTool: PracticeTool;
   setPracticeTool: (practiceTool: PracticeTool) => void;
+  practiceToolColor: string;
 }>({
   practiceTool: "",
   setPracticeTool: (arg1: PracticeTool) => {},
+  practiceToolColor: "#FFFFFF",
 });
 
 export function PracticeToolProvider({ children }: { children: ReactNode }) {
   const [practiceTool, setPracticeTool] = useState<PracticeTool>("textEditor");
+
+  const getPracticeToolColor = () => {
+    switch (practiceTool) {
+      case "spreadsheet":
+        return "#72D6BA";
+      case "textEditor":
+        return "#33C6FD";
+      default:
+        return "#FFFFFF";
+    }
+  };
+  const practiceToolColor = getPracticeToolColor();
   return (
-    <PracticeToolContext.Provider value={{ practiceTool, setPracticeTool }}>
+    <PracticeToolContext.Provider
+      value={{ practiceTool, setPracticeTool, practiceToolColor }}
+    >
       {children}
     </PracticeToolContext.Provider>
   );
