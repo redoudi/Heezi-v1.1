2bc513ba93a06815cdc1b54118c406009c35eecc
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-18 10:28:47 +0100

    k

--- components/practice-tools/spreadsheet/grid/spreadsheet-grid.tsx
+++ components/practice-tools/spreadsheet/grid/spreadsheet-grid.tsx
@@ -5,50 +5,45 @@ import HeaderRow from "./header-row";
 
 const COLUMNS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"];
 // Row configuration
-const ROWS = Array.from({ length: 18 }, (_, i) => ({
-  id: (i + 1).toString(),
-  rowNumber: i + 1,
-}));
+const ROWS = Array.from({ length: 18 }, (_, i) => i + 1);
 
 export default function SpreadsheetGrid() {
   useKbdSpdshtNextRow();
   // Header component
 
   // Row component
-  const renderRow = ({
-    item,
-    index,
-  }: {
-    item: (typeof ROWS)[0];
-    index: number;
-  }) => {
-    return (
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        style={styles.scrollView}
-      >
-        <View style={styles.rowNumberContainer}>
-          <Text style={styles.rowNumber}>{item.id}</Text>
-        </View>
-        {COLUMNS.map((_, colIndex) => (
-          <Cell
-            key={`${String.fromCharCode(65 + colIndex)}${index + 1}`}
-            id={`${String.fromCharCode(65 + colIndex)}${index + 1}`}
-          />
-        ))}
-      </ScrollView>
-    );
-  };
 
   return (
     <View style={styles.column12}>
       <HeaderRow columnsLetters={COLUMNS} />
-
       <FlatList
         data={ROWS}
-        renderItem={renderRow}
-        keyExtractor={(item) => item.id}
+        renderItem={({
+          item,
+          index,
+        }: {
+          item: (typeof ROWS)[0];
+          index: number;
+        }) => {
+          return (
+            <ScrollView
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              style={styles.gridCellsContainer}
+            >
+              <View style={styles.rowNumberContainer}>
+                <Text style={styles.rowNumber}>{item}</Text>
+              </View>
+              {COLUMNS.map((_, colIndex) => (
+                <Cell
+                  key={`${String.fromCharCode(65 + colIndex)}${index + 1}`}
+                  id={`${String.fromCharCode(65 + colIndex)}${index + 1}`}
+                />
+              ))}
+            </ScrollView>
+          );
+        }}
+        keyExtractor={(item) => item.toString()}
         showsVerticalScrollIndicator={true}
       />
     </View>
@@ -64,8 +59,7 @@ const styles = StyleSheet.create({
     marginBottom: 17,
     marginHorizontal: 8,
   },
-  scrollView: {
-    flexDirection: "row",
+  gridCellsContainer: {
     paddingHorizontal: 8,
     marginBottom: 8,
   },
