b33e46de42b353cd9a8bf95d8b678d00f28bd87c
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-19 11:43:41 +0100

    k

--- components/home/level-header.tsx
+++ components/home/level-header.tsx
@@ -7,7 +7,7 @@ interface LevelHeaderProps {
 export function LevelHeader({ onScorePress }: LevelHeaderProps) {
   return (
     <LevelSection
-      level="Niveau. 1"
+      level="Niveau : débutant"
       score="500"
       coinIconUri={require("@/assets/images/c30140rw_expires_30_days.png")}
       badgeIconUri={require("@/assets/images/a8a1zbel_expires_30_days.png")}
