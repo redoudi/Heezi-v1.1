e8e9a1f151e87a995c5c2c80077c057359ee19c3
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-19 10:04:23 +0100

    k

--- hooks/usePracticeToolConstants.ts
+++ hooks/usePracticeToolConstants.ts
@@ -2,10 +2,22 @@ import practiceToolsConstants from "@/constants/practiceToolsConstants";
 import { PracticeTool } from "@/context/usePracticeTool";
 import { useLocalSearchParams } from "expo-router";
 
+const DEFAULT_PRACTICE_TOOL: PracticeTool = "spreadsheet";
+
 export default function usePracticeToolConstants() {
-  const { practiceTool } =
-    useLocalSearchParams<{ practiceTool: PracticeTool }>() || "spreadsheet";
+  const params = useLocalSearchParams<{ practiceTool?: PracticeTool }>();
+  const paramValue = params.practiceTool as PracticeTool | undefined;
+
+  const practiceTool = (
+    paramValue &&
+    Object.prototype.hasOwnProperty.call(practiceToolsConstants, paramValue)
+      ? paramValue
+      : DEFAULT_PRACTICE_TOOL
+  ) satisfies PracticeTool;
+
   const toolConstants =
-    practiceToolsConstants[practiceTool as keyof typeof practiceToolsConstants];
+    practiceToolsConstants[practiceTool] ??
+    practiceToolsConstants[DEFAULT_PRACTICE_TOOL];
+
   return { practiceTool, toolConstants };
 }
