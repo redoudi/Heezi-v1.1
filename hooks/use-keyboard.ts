1f1b94aa2f383b4ac2929721c30b8e93929414db
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-20 18:45:20 +0100

    k

--- hooks/use-keyboard.ts
+++ hooks/use-keyboard.ts
@@ -1,5 +1,3 @@
-import useSpreadsheetStore from "@/store/useSpreadsheetStore";
-import { getCellUnderneath } from "@/utils/spreadsheetUtils";
 import { useEffect } from "react";
 
 export const useKbdNextStep = ({
@@ -30,33 +28,4 @@ export const useKbdNextStep = ({
       }
     };
   }, [levelType]);
-};
-
-export const useKbdSpdshtNextRow = () => {
-  const {
-    spreadsheetData,
-    setCellsSelected,
-  } = useSpreadsheetStore();
-  const cellsSelected = spreadsheetData?.cellsSelected || [];
-  useEffect(() => {
-    const handleKeyPress = (event: KeyboardEvent) => {
-      if (cellsSelected.length > 0 && event.key === "Enter") {
-        const firstCell = cellsSelected[0];
-        const nextCell = getCellUnderneath(firstCell);
-        setCellsSelected([nextCell]);
-      }
-    };
-
-    // Add event listener
-    if (typeof window !== "undefined") {
-      window.addEventListener("keydown", handleKeyPress);
-    }
-
-    // Cleanup
-    return () => {
-      if (typeof window !== "undefined") {
-        window.removeEventListener("keydown", handleKeyPress);
-      }
-    };
-  }, [cellsSelected, setCellsSelected]);
 };
