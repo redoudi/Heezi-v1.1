30b74478f7c740c6fb827ed473b9cb696e6755b2
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-22 17:06:45 +0100

    k

--- constants/sectionButtons.ts
+++ constants/sectionButtons.ts
@@ -1,12 +1,12 @@
 export default {
   spreadsheet: {
     lesson: require("@/assets/images/buttonIcons/lessonVert.png"),
-    quiz: require("@/assets/images/buttonIcons/quiz.png"),
-    practice: require("@/assets/images/buttonIcons/practice.png"),
+    quiz: require("@/assets/images/buttonIcons/quizVert.png"),
+    practice: require("@/assets/images/buttonIcons/practiceVert.png"),
   },
   textEditor: {
     lesson: require("@/assets/images/buttonIcons/lessonBleu.png"),
-    quiz: require("@/assets/images/buttonIcons/quiz.png"),
-    practice: require("@/assets/images/buttonIcons/practice.png"),
+    quiz: require("@/assets/images/buttonIcons/quizBleu.png"),
+    practice: require("@/assets/images/buttonIcons/practiceBleu.png"),
   },
 };
