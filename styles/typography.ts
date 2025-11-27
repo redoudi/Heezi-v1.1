8c7e1523fd271507d4a8e24466cf7ef338e4b10c
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-27 10:39:47 +0100

    Update typography styles to adjust font sizes and weights for improved readability and consistency across the application.

--- styles/typography.ts
+++ styles/typography.ts
@@ -25,18 +25,18 @@ const base = (
 });
 
 export const typography = StyleSheet.create({
-  display: base(30, { lineHeight: 32, fontWeight: "bold" }),
-  title: base(26),
-  titleBold: base(26, { fontWeight: "bold" }),
-  headline: base(22, { fontWeight: "bold" }),
-  bodyLarge: base(18),
-  bodyLargeBold: base(18, { fontWeight: "bold" }),
-  body: base(14, { lineHeight: 24 }),
-  bodySemiBold: base(14, { lineHeight: 24, fontWeight: "600" }),
-  bodyBold: base(14, { fontWeight: "bold" }),
-  caption: base(12),
-  captionBold: base(12, { fontWeight: "bold" }),
-  captionSmall: base(10),
+  display: base(28, { lineHeight: 32, fontWeight: "bold" }),
+  title: base(24),
+  titleBold: base(24, { fontWeight: "bold" }),
+  headline: base(20, { fontWeight: "bold" }),
+  bodyLarge: base(16),
+  bodyLargeBold: base(16, { fontWeight: "bold" }),
+  body: base(12, { lineHeight: 24 }),
+  bodySemiBold: base(12, { lineHeight: 24, fontWeight: "600" }),
+  bodyBold: base(12, { fontWeight: "bold" }),
+  caption: base(10),
+  captionBold: base(10, { fontWeight: "bold" }),
+  captionSmall: base(8),
 });
 
 export type TypographyVariant = keyof typeof typography;
