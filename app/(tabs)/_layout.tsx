15820ab4865a48315c3e7d7ea19283cbaa123cf4
redoudi <135809071+redoudi@users.noreply.github.com>
2025-11-27 11:39:38 +0100

    k

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,10 +4,12 @@ import { HapticTab } from "@/components/haptic-tab";
 import { Colors } from "@/constants/theme";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import { typography } from "@/styles/typography";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, View, useWindowDimensions } from "react-native";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const { width } = useWindowDimensions();
+  const isCompact = width < 768;
 
   return (
     <Tabs
@@ -15,21 +17,36 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-        tabBarPosition: "left",
-        tabBarStyle: styles.tabBarStyle,
-        tabBarItemStyle: styles.tabBarItemStyle,
-        tabBarIconStyle: styles.tabBarIconStyle,
-        tabBarLabelStyle: [typography.headline, styles.tabBarLabelStyle],
-
-        tabBarBackground: () => (
-          <View>
-            <Image
-              source={require("../../assets/images/logo.png")}
-              resizeMode="contain"
-              style={styles.logoStyle}
-            />
-          </View>
-        ),
+        tabBarPosition: isCompact ? "bottom" : "left",
+        tabBarStyle: [
+          styles.tabBarStyle,
+          isCompact && styles.tabBarStyleCompact,
+        ],
+        tabBarItemStyle: [
+          styles.tabBarItemStyle,
+          isCompact && styles.tabBarItemStyleCompact,
+        ],
+        tabBarIconStyle: [
+          styles.tabBarIconStyle,
+          isCompact && styles.tabBarIconStyleCompact,
+        ],
+        tabBarLabelStyle: [
+          typography.headline,
+          styles.tabBarLabelStyle,
+          isCompact && styles.tabBarLabelStyleCompact,
+        ],
+        tabBarBackground: () =>
+          isCompact ? (
+            <View />
+          ) : (
+            <View>
+              <Image
+                source={require("../../assets/images/logo.png")}
+                resizeMode="contain"
+                style={styles.logoStyle}
+              />
+            </View>
+          ),
       }}
     >
       <Tabs.Screen
@@ -40,7 +57,10 @@ export default function TabLayout() {
             <Image
               source={require("../../assets/images/play.png")}
               resizeMode="stretch"
-              style={styles.tabBarItemIconImage}
+              style={[
+                styles.tabBarItemIconImage,
+                isCompact && styles.tabBarItemIconImageCompact,
+              ]}
             />
           ),
         }}
@@ -53,7 +73,10 @@ export default function TabLayout() {
             <Image
               source={require("../../assets/images/profil.png")}
               resizeMode="stretch"
-              style={styles.tabBarItemIconImage}
+              style={[
+                styles.tabBarItemIconImage,
+                isCompact && styles.tabBarItemIconImageCompact,
+              ]}
             />
           ),
         }}
@@ -66,7 +89,10 @@ export default function TabLayout() {
             <Image
               source={require("../../assets/images/score.png")}
               resizeMode="stretch"
-              style={styles.tabBarItemIconImage}
+              style={[
+                styles.tabBarItemIconImage,
+                isCompact && styles.tabBarItemIconImageCompact,
+              ]}
             />
           ),
         }}
@@ -79,7 +105,10 @@ export default function TabLayout() {
             <Image
               source={require("../../assets/images/succes.png")}
               resizeMode="stretch"
-              style={styles.tabBarItemIconImage}
+              style={[
+                styles.tabBarItemIconImage,
+                isCompact && styles.tabBarItemIconImageCompact,
+              ]}
             />
           ),
         }}
@@ -107,6 +136,20 @@ const styles = StyleSheet.create({
     maxWidth: 193,
     margin: 32,
   },
+  tabBarStyleCompact: {
+    paddingTop: 0,
+    paddingRight: 0,
+    width: "100%",
+    minWidth: "100%",
+    maxWidth: "100%",
+    margin: 0,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderColor: "#E0E0E0",
+    flexDirection: "row",
+    height: 72,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
   tabBarBackgroundStyle: {
     alignItems: "center",
     justifyContent: "center",
@@ -121,15 +164,32 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     marginVertical: 15,
   },
+  tabBarItemStyleCompact: {
+    marginVertical: 0,
+    flexDirection: "column",
+    flex: 1,
+  },
   tabBarIconStyle: {
     marginRight: 14,
+  },
+  tabBarIconStyleCompact: {
+    marginRight: 0,
+    marginBottom: 4,
   },
 
   tabBarLabelStyle: {
     color: "black",
   },
+  tabBarLabelStyleCompact: {
+    color: "black",
+    fontSize: 12,
+  },
   tabBarItemIconImage: {
     width: 48,
     height: 48,
+  },
+  tabBarItemIconImageCompact: {
+    width: 32,
+    height: 32,
   },
 });
