339ad169f963c8657e1f9060b0e877c3fb905970
redoudi <135809071+redoudi@users.noreply.github.com>
2025-12-04 13:27:13 +0100

    w

--- components/home/level-section.tsx
+++ components/home/level-section.tsx
@@ -33,6 +33,7 @@ const styles = StyleSheet.create({
     paddingVertical: 16,
     marginBottom: !isMobile ? 32 : 16,
     paddingHorizontal: 16,
+    flexDirection: !isMobile ? "column" : "row",
   },
   levelText: {
     fontSize: 18,
