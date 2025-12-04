faf0780577e1af70e428190f12cc46b8caf1c897
redoudi <135809071+redoudi@users.noreply.github.com>
2025-12-04 11:34:58 +0100

    cln

--- components/home/right-column.tsx
+++ components/home/right-column.tsx
@@ -1,25 +1,12 @@
 import { CategoryList } from "@/components/home/category-list";
 import { LevelSection } from "@/components/home/level-section";
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 
 export function RightColumn() {
   return (
-    <View style={styles.rightColumn}>
-      <LevelSection
-        level="Niveau : débutant"
-        score="0"
-        coinIconUri={require("@/assets/images/c30140rw_expires_30_days.png")}
-        badgeIconUri={require("@/assets/images/a8a1zbel_expires_30_days.png")}
-        onScorePress={() => {}}
-      />
+    <View>
+      <LevelSection />
       <CategoryList />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  rightColumn: {
-    alignSelf: "flex-start",
-    backgroundColor: "#FFFFFF",
-  },
-});
