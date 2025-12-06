4a7c7ca297b75095979e7689b2341dee473bd8be
redoudi <135809071+redoudi@users.noreply.github.com>
2025-12-06 20:38:11 +0100

    k

--- components/practice-tools/spreadsheet/function-bar.tsx
+++ components/practice-tools/spreadsheet/function-bar.tsx
@@ -1,3 +1,4 @@
+import { isMobile } from "@/utils/isMobile";
 import {
   Image,
   StyleSheet,
@@ -9,32 +10,32 @@ import {
 
 export default function FunctionBar() {
   return (
-    <View style={styles.row4}>
-      <TouchableOpacity style={styles.buttonRow7} onPress={() => {}} disabled>
+    <View style={styles.mainContainer}>
+      <TouchableOpacity style={styles.cellDropdown} onPress={() => {}} disabled>
         <Text style={styles.text5}>{"A20"}</Text>
         <Image
           source={require("@/assets/images/w85265az_expires_30_days.png")}
           resizeMode={"stretch"}
-          style={styles.image12}
+          style={styles.triangleIcon}
         />
       </TouchableOpacity>
-      <View style={styles.row13}>
+      <View style={styles.fxRow}>
         <Image
           source={require("@/assets/images/cmx9ciy9_expires_30_days.png")}
           resizeMode={"stretch"}
-          style={styles.image31}
+          style={styles.xIcon}
         />
         <Image
           source={require("@/assets/images/2cz9arwn_expires_30_days.png")}
           resizeMode={"stretch"}
-          style={styles.image32}
+          style={styles.tickIcon}
         />
-        <View style={styles.row14}>
-          <Text style={styles.text8}>{"fx"}</Text>
+        <View style={styles.fxDropdown}>
+          <Text style={styles.fxText}>{"fx"}</Text>
           <Image
             source={require("@/assets/images/fp37iklk_expires_30_days.png")}
             resizeMode={"stretch"}
-            style={styles.image33}
+            style={styles.triangleIcon}
           />
         </View>
       </View>
@@ -44,12 +45,13 @@ export default function FunctionBar() {
 }
 
 const styles = StyleSheet.create({
-  row4: {
+  mainContainer: {
     flexDirection: "row",
     backgroundColor: "#FFFFFF",
-    paddingVertical: 8,
+    paddingHorizontal: isMobile ? 16 : 32,
+    gap: 8,
   },
-  buttonRow7: {
+  cellDropdown: {
     width: 88,
     flexDirection: "row",
     justifyContent: "space-between",
@@ -57,62 +59,55 @@ const styles = StyleSheet.create({
     backgroundColor: "#EFEFEF",
     borderRadius: 8,
     paddingVertical: 8,
-    marginLeft: 32,
-    marginRight: 8,
   },
   text5: {
     fontSize: 10,
     color: "#292929",
     marginLeft: 8,
   },
-  image12: {
+  triangleIcon: {
     borderRadius: 8,
     width: 9,
     height: 6,
-    marginRight: 8,
   },
-  row13: {
+  fxRow: {
     width: 96,
     flexDirection: "row",
     alignItems: "center",
     backgroundColor: "#EFEFEF",
     borderRadius: 8,
     paddingVertical: 8,
-    marginRight: 8,
   },
-  image31: {
+  xIcon: {
     borderRadius: 8,
     width: 12,
     height: 12,
     marginLeft: 8,
     marginRight: 10,
   },
-  image32: {
+  tickIcon: {
     borderRadius: 8,
     width: 19,
     height: 14,
     marginRight: 10,
   },
-  row14: {
+  fxDropdown: {
     width: 29,
     flexDirection: "row",
     alignItems: "center",
     marginRight: 8,
   },
-  text8: {
-    fontSize: 10,
+  fxText: {
+    fontSize: 12,
     color: "#292929",
     marginRight: 6,
   },
-  image33: {
-    width: 11,
-    height: 7,
-  },
+
   input: {
     flex: 1,
     borderRadius: 8,
-    height: 24,
-    marginRight: 8,
+    marginVertical: 2,
+
     backgroundColor: "#EFEFEF",
   },
 });
