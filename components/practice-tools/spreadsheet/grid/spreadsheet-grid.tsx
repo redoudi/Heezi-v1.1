3e0a207dfa5c2807cd9bad99f9b66cb88203d34e
redoudi <135809071+redoudi@users.noreply.github.com>
2025-12-10 20:18:11 +0100

    k

--- components/practice-tools/spreadsheet/grid/spreadsheet-grid.tsx
+++ components/practice-tools/spreadsheet/grid/spreadsheet-grid.tsx
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
     marginLeft: 8,
   },
   rowNumber: {
-    fontSize: 10,
+    fontSize: 12,
     fontWeight: "bold",
     color: "#292929",
     textAlign: "center",
